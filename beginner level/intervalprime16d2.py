c7940abb87b873ff9c7cd3a53ff8272c57ba1ee5
harishk-97 <37336319+harishk-97@users.noreply.github.com>
2018-04-29 14:35:22 +0530

    Create intervalprime16d2.py

---
+++ beginner level/intervalprime16d2.py
@@ -0,0 +1,17 @@
+z=input().split()
+z=list(map(int,z))
+z[0]=z[0]+1
+m=0
+while(z[0]<z[1]):
+  n=0
+  for i in range(z[0]):
+    if(i>1):
+      if(z[0]%i==0):
+        n=1 
+  if(n==0):
+    if(m==0):
+      print(z[0],end="")
+      m=1
+    else:
+      print("",z[0],end="")
+  z[0]=z[0]+1
