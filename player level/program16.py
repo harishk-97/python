255487b347b3db86c96911ccb35dd0a9e663e27f
harishk-97 <37336319+harishk-97@users.noreply.github.com>
2018-05-11 23:08:10 +0530

    Create program16.py

---
+++ player level/program16.py
@@ -0,0 +1,18 @@
+f=input()
+a=input().split()
+a=sorted(a)
+b=[]
+for i in range(len(a)):
+  if a[i] not in b:
+    b.append(a[i])
+d=[]
+for i in range(len(b)):
+  c=0
+  for j in range(len(a)):
+    if(b[i]==a[j]):
+      c+=1
+  d.append(c)
+e=d.index(1)
+print(b[e])
+  
+  
