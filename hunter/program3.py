4fc064cbbfb4308d59e29c57294521572860e8a3
harishk-97 <37336319+harishk-97@users.noreply.github.com>
2018-05-11 13:11:53 +0530

    Update program3.py

--- hunter/program3.py
+++ hunter/program3.py
@@ -5,11 +5,14 @@ c=[]
 j=1
 for i in range(a):
   if(i==b[i]):
-    if j==1:
-      print(i,end='')
-      j=0
-    else:
-      print('',i,end='')
-      
-      
-  
+    c.append(i)
+c=sorted(c)
+for i in range(len(c)):
+  if j==1:
+    print(c[i],end='')
+    j=0
+  else:
+    print('',c[i],end='')
+if(len(c)==0):
+  print('-1')
+ 
