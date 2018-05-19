5d59f90d078b807c4aea05a46df2373622540235
harishk-97 <37336319+harishk-97@users.noreply.github.com>
2018-05-19 22:59:34 +0530

    Update program7.py

--- hunter/program7.py
+++ hunter/program7.py
@@ -1,20 +1,17 @@
-gun=int(input())
+kun=int(input())
 sun=input().split()
 sun=list(map(int,sun))
 m=0
-for i in range(gun):
-  if(i%2==0 and sun[i]%2==1):
+for n in range(kun):
+  if(n%2==0 and sun[n]%2==1):
     if(m==0):
-      print(sun[i],end='')
+      print(sun[n],end='')
       m=1
     else:
-      print('',sun[i],end='')
-  if(i%2==1 and sun[i]%2==0):
+      print('',sun[n],end='')
+  if(n%2==1 and sun[n]%2==0):
     if(m==0):
-      print(sun[i],end='')
+      print(sun[n],end='')
       m=1
     else:
-      print('',sun[i],end='')
-
-
-    
+      print('',sun[n],end='')
