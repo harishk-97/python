f40e9b0d712f88b0f28dfcb0e99e3054315ab815
harishk-97 <37336319+harishk-97@users.noreply.github.com>
2018-06-01 20:26:20 +0530

    Create program62.py

---
+++ pro level/program62.py
@@ -0,0 +1,20 @@
+milo=input()
+kilo=len(milo)
+def balin(a):
+    b=len(a)
+    c=[]
+    for i in range(b+1):
+        for j in range(b+1):
+            if(j>i):
+                d=a[i:j]
+                if(d==d[::-1]):
+                    if d not in c:
+                        c.append(d)
+    e=[]
+    for i in c:
+        e.append(len(i))
+    f=max(e)
+    g=e.index(f)
+    h=c[g]
+    return(len(h))
+print(kilo-balin(milo))
