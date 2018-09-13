0c369fc28f66be4ea9a1225119b44a8552003de7
harishk-97 <37336319+harishk-97@users.noreply.github.com>
2018-09-14 03:17:33 +0530

    Update program68.py

--- pro level/program68.py
+++ pro level/program68.py
@@ -1,3 +1,12 @@
 z=input().split()
 z=list(map(int,z))
-print('1',z[0]-z[1])
+if(z[1]!=z[0]):
+  print('1',end=' ')
+if((z[0]-z[1])<(2*z[1])):
+  print(z[0]-z[1])
+else:
+  if(z[1]==0):
+    print("0",end=' ')
+    print('0')
+  else:
+    print(2*z[1])
