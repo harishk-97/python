3ed509c2885edf5fb2331cafdad916e2d76793ef
harishk-97 <37336319+harishk-97@users.noreply.github.com>
2018-12-15 01:52:50 +0530

    Create program18.py

---
+++ pro level/program18.py
@@ -0,0 +1,27 @@
+def checker(a,b,i,j):
+  for k in range(min((a[0]-i),(a[1]-j))):
+    for l in range(i,k+i+1):
+      for m in range(j,k+j+1):
+        if(b[l][m]==0):
+          return(k)
+  return(k+1)
+
+a=list(map(int,input().split()))
+b=[]
+flag=0
+for i in range(a[0]):
+  b.append(list(map(int,input().split())))
+for i in range(a[0]):
+  for j in range(a[1]):
+    c=b[i][j]
+    if(c==1):
+      z=checker(a,b,i,j)
+      if(flag<z):
+        flag=z
+for i in range(flag):
+  for j in range(flag):
+    if((flag-1)==j):
+      print(1)
+    else:
+      print(1,end=' ')
+
