99c5445fe3bb7976288b8f72f520f3dc77d75be7
harishk-97 <37336319+harishk-97@users.noreply.github.com>
2018-12-19 19:36:40 +0530

    Create program23.py

---
+++ player level/program23.py
@@ -0,0 +1,12 @@
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=list(map(int,input().split()))
+flag=0
+for i in range(a[1]):
+  b.append(c[i])
+  if(flag==0):
+    print(max(b),end='')
+    flag+=1
+  else:
+    print('',max(b),end='')
+
